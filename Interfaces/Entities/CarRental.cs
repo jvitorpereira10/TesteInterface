c690957d5f637aad9990db377e5ed437e16a4bf3
João Santos <jvitorpereira10@gmail.com>
2022-06-14 21:56:27 -0300

    Implementacao da interface e servicos

---
+++ Interfaces/Entities/CarRental.cs
@@ -0,0 +1,19 @@
+﻿namespace Interfaces.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+           // Invoice = null; // O objeto Invoice será instanciado apenas quando a nota fiscal for processada.
+                            // Sendo assim, não há a necessidade de passar este objeto no construtor como null, pois ele será instanciado como nulo por padrão;
+        }
+    }
+}
